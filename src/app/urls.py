f00ae62c3a303ffc2c0134da806d3afd580d8e9e
Uladzimir Fiodarau <ufiodarau@gmail.com>
2023-02-23 14:33:03 +0400

    Added a quick fix for django-debug-toolbar

--- src/app/urls.py
+++ src/app/urls.py
@@ -14,8 +14,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.urls import include
 from django.urls import path
+
+from app.conf.environ import env
 
 urlpatterns = [
     path("admin/", admin.site.urls),
 ]
+
+if env("DEBUG", cast=bool, default=False):
+    import debug_toolbar
+
+    urlpatterns.append(path("__debug__/", include(debug_toolbar.urls)))
